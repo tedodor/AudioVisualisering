6b6f366fdc953d35e49ad306e9f7bc63fea424a7
Teodor <teodor@bedford.st>
2019-04-05 09:51:16 +0200

    asdf

--- model.py
+++ model.py
@@ -1,66 +1,58 @@
-import math, random, time
-import audio, view
+import math, random, time, pickle
+import player, view, extractor
 
-THRESH = 100
+THRESH = 25
 
-class Point:
-    def __init__(self, x, y, clip):
-        self.x = x
-        self.y = y
-        self.clip = clip
-
-    def dist(self, x, y):
-        return math.sqrt((x - self.x)**2 + (y - self.y)**2)
-
-    
 class Model:
     points = []
-    def __init__(self, clips1, clips2):
-        self.v = view.View(THRESH)
-        self.fill(clips1, 0)
-        self.fill(clips2, 250)
-        self.draw()
-        self.player = audio.Player(self.get_clips)
+    def __init__(self):
+        self.threshold = THRESH
+        self.v = view.View(self.threshold)
+        self.points = []
+
+    def add_points(self, points):
+        self.points += points
         
-    def fill(self, clips, i):
-        self.points += [Point(random.randint(0, 250)+i,random.randint(0, 500),c) for c in clips]
-
     def start(self):
-        pass
-
+        self.draw()
+        self.player = player.Player(self.get_clips)
+        
     def draw(self):
         self.v.draw(self.points)
         
     def get_clips(self):
         x, y = self.v.get_mouse_pos()
         for p in self.points:
-            if p.dist(x, y) < THRESH:
-                yield p.clip
+            if p.dist(x, y) < self.threshold and p.clips:
+                yield p.clips
     
 
 
 if __name__=='__main__':
-    CHUNK = 8000
-    import wave, pyaudio
+
+    import pyaudio
+
     p = pyaudio.PyAudio()
+    m = []
 
-    wf = wave.open("rsc/outF.wav", 'rb')
-    fr = wf.getframerate()
-    data1 = []
-    d = wf.readframes(CHUNK)
-    while len(d) > 0:
-        data1 += [d]
-        d = wf.readframes(CHUNK)
+    #Alice
+    try:
+        for i in range(1, 2):
+            path = "/Volumes/A14SAMGR/TaltekDT2112/Moby/"
+            points = extractor.extract_features(path + "c{}.wav".format(i),
+                                                path + "analysis/c{}.f0".format(i))
+            m+=points
+    except:
+        pass
 
+    pickle.dump(m, open("/Volumes/A14SAMGR/TaltekDT2112/out2.pickle", "wb"))
+    # for i in range(1,4):
+    #     path = "/Volumes/A14SAMGR/TaltekDT2112/Crusoe/"
+    #     points = extractor.extract_features(path + "crusoe0{}.wav".format(i),
+    #                                         path + "Analys/crusoe0{}.f0".format(i))
+    #     m.add_points(points)
 
-    wf = wave.open("rsc/outM.wav", 'rb')
-    fr = wf.getframerate()
-    data2 = []
-    d = wf.readframes(CHUNK)
-    while len(d) > 0:
-        data2 += [d]
-        d = wf.readframes(CHUNK)
-
-    m = Model(data1, data2)
-
-    m.v.w.mainloop()
+    
+    # m.start()
+    # m.v.w.mainloop()
+    
--- view.py
+++ view.py
@@ -3,23 +3,26 @@ import tkinter as tk, random
 WINDOW_SIZE = (500,500)
 CIRCLE_SIZE = 10
 CURSOR_SIZE = 50
+root = tk.Tk()
 
 class View:
     def __init__(self, threshold):
-        self.root = tk.Tk()
+        
         self.thresh = threshold
-        self.w = tk.Canvas(self.root, width=WINDOW_SIZE[0], height=WINDOW_SIZE[1])
-        self.w.create_rectangle(0, 0, 1000, 1000, fill="white")
+        self.w = tk.Canvas(root, width=WINDOW_SIZE[0], height=WINDOW_SIZE[1])
+        self.w.create_rectangle(0, 0, WINDOW_SIZE[0]*2, WINDOW_SIZE[1]*2, fill="white")
         self.w.pack()
         self.x = 0
         self.y = 0
         self.cursor = self.w.create_oval(self.x-threshold,self.y-threshold,self.x+threshold,self.y+threshold,outline="red")
-        self.root.bind("<Motion>", self.motion)
+        root.bind("<Motion>", self.motion)
+        #        self.root.bind("<MouseWheel>", self.scroll)
         
     def draw_circle(self, x, y):
         self.w.create_oval(x,y,x+CIRCLE_SIZE,y+CIRCLE_SIZE,outline="blue",fill="blue")
 
     def draw(self,points):
+        print("drawing {} points".format(len(points)))
         for p in points:
             self.draw_circle(p.x,p.y)
 
@@ -27,7 +30,6 @@ class View:
         return (self.x, self.y)
 
     def motion(self, event):
-        print(event.x, event.y)
         self.w.move(self.cursor, event.x - self.x , event.y - self.y)
         self.x, self.y = event.x, event.y
 
